2ef0d946e5aa294fb1d3f43d1b24e43d090d2755
dz-coder <dzcoder@hotmail.fr>
2014-11-19 23:07:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,133 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('app', [
+    'ngAnimate',
+    'ngCookies',
+    'ngStorage',
+    'ui.router',
+    'ui.bootstrap',
+    'ui.load',
+    'ui.jq',
+    'ui.validate',
+    'pascalprecht.translate',
+    'app.filters',
+    'app.services',
+    'app.directives',
+    'app.controllers'
+  ])
+.run(
+  [          '$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;        
+    }
+  ]
+)
+.config(
+  [          '$stateProvider', '$urlRouterProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+    function ($stateProvider,   $urlRouterProvider,   $controllerProvider,   $compileProvider,   $filterProvider,   $provide) {
+        
+        // lazy controller, directive and service
+        app.controller = $controllerProvider.register;
+        app.directive  = $compileProvider.directive;
+        app.filter     = $filterProvider.register;
+        app.factory    = $provide.factory;
+        app.service    = $provide.service;
+        app.constant   = $provide.constant;
+        app.value      = $provide.value;
+
+        $urlRouterProvider
+            .otherwise('/app/dashboard');
+        $stateProvider            
+            .state('app', {
+                abstract: true,
+                url: '/app',
+                templateUrl: 'ui/app.html'
+            })
+            .state('app.dashboard', {
+                url: '/dashboard',
+                templateUrl: 'ui/dashboard.php'
+            })
+            .state('app.widgets', {
+                url: '/widgets',
+                templateUrl: 'ui/ui_widgets.php'
+            })
+            .state('app.files', {
+                url: '/files'+Math.random(),
+                templateUrl: 'ui/files.php',
+				
+
+            })
+
+    }
+  ]
+)
+
+.config(['$translateProvider', function($translateProvider){
+
+  // Register a loader for the static files
+  // So, the module will search missing translation tables under the specified urls.
+  // Those urls are [prefix][langKey][suffix].
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'l10n/',
+    suffix: '.json'
+  });
+
+  // Tell the module what language to use by default
+  $translateProvider.preferredLanguage('en');
+
+  // Tell the module to store the language in the local storage
+  $translateProvider.useLocalStorage();
+
+}])
+
+/**
+ * jQuery plugin config use ui-jq directive , config the js and css files that required
+ * key: function name of the jQuery plugin
+ * value: array of the css js file located
+ */
+.constant('JQ_CONFIG', {
+    easyPieChart:   ['js/jquery/charts/easypiechart/jquery.easy-pie-chart.js'],
+    sparkline:      ['js/jquery/charts/sparkline/jquery.sparkline.min.js'],
+    plot:           ['js/jquery/charts/flot/jquery.flot.min.js', 
+                        'js/jquery/charts/flot/jquery.flot.resize.js',
+                        'js/jquery/charts/flot/jquery.flot.tooltip.min.js',
+                        'js/jquery/charts/flot/jquery.flot.spline.js',
+                        'js/jquery/charts/flot/jquery.flot.orderBars.js',
+                        'js/jquery/charts/flot/jquery.flot.pie.min.js'],
+    slimScroll:     ['js/jquery/slimscroll/jquery.slimscroll.min.js'],
+    sortable:       ['js/jquery/sortable/jquery.sortable.js'],
+    nestable:       ['js/jquery/nestable/jquery.nestable.js',
+                        'js/jquery/nestable/nestable.css'],
+    filestyle:      ['js/jquery/file/bootstrap-filestyle.min.js'],
+    slider:         ['js/jquery/slider/bootstrap-slider.js',
+                        'js/jquery/slider/slider.css'],
+    chosen:         ['js/jquery/chosen/chosen.jquery.min.js',
+                        'js/jquery/chosen/chosen.css'],
+    TouchSpin:      ['js/jquery/spinner/jquery.bootstrap-touchspin.min.js',
+                        'js/jquery/spinner/jquery.bootstrap-touchspin.css'],
+    wysiwyg:        ['js/jquery/wysiwyg/bootstrap-wysiwyg.js',
+                        'js/jquery/wysiwyg/jquery.hotkeys.js'],
+    dataTable:      ['js/jquery/datatables/jquery.dataTables.min.js',
+                        'js/jquery/datatables/dataTables.bootstrap.js',
+                        'js/jquery/datatables/dataTables.bootstrap.css'],
+    vectorMap:      ['js/jquery/jvectormap/jquery-jvectormap.min.js', 
+                        'js/jquery/jvectormap/jquery-jvectormap-world-mill-en.js',
+                        'js/jquery/jvectormap/jquery-jvectormap-us-aea-en.js',
+                        'js/jquery/jvectormap/jquery-jvectormap.css'],
+    footable:       ['js/jquery/footable/footable.all.min.js',
+                        'js/jquery/footable/footable.core.css']
+    }
+)
+
+
+.constant('MODULE_CONFIG', {
+    select2:        ['js/jquery/select2/select2.css',
+                        'js/jquery/select2/select2-bootstrap.css',
+                        'js/jquery/select2/select2.min.js',
+                        'js/modules/ui-select2.js']
+    }
+)
+;
